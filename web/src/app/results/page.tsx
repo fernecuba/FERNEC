3c4ac2f80975edab9af682e04457bda2ba358dd0
Cota <cotarelorodrigo@gmail.com>
2024-05-28 10:52:52 -0300

    add results dashboard

--- web/src/app/results/page.tsx
+++ web/src/app/results/page.tsx
@@ -1,3 +1,34 @@
+import { BarChartEmotions } from "./BarChartEmotions";
+
 export default function Results() {
-  return <main className="flex h-screen flex-col p-24">Results</main>;
+  return (
+    <main className="flex h-screen flex-col">
+      <div className="container bg-gray-200 ">
+        <h2 className="text-4xl font-bold p-4">
+          Here <span className="text-green-700"> are </span>your
+          <span className="text-green-700"> Results</span>
+        </h2>
+        <div className="pt-10 flex flex-row">
+          <div className="w-1/2">
+            <BarChartEmotions />
+          </div>
+          <div className="w-1/2 flex flex-col justify-around pl-14">
+            <p className="font-bold">Your video has 60 seconds long</p>
+            <p className="font-bold">
+              for 20 seconds you looked{" "}
+              <span className="text-green-600">happy</span>
+            </p>
+            <p className="font-bold">
+              for 15 seconds you looked{" "}
+              <span className="text-blue-300">neutral</span>
+            </p>
+            <p className="font-bold">
+              for 10 seconds you looked{" "}
+              <span className="text-yellow-600">surprised</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
