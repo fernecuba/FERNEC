f47cacaf340b00a1b60486c9db55d7837e8b7437
Cota <cotarelorodrigo@gmail.com>
2024-05-28 13:13:51 -0300

    remove unused comment

--- web/src/app/demo/Questions.tsx
+++ web/src/app/demo/Questions.tsx
@@ -52,28 +52,4 @@ export default function Questions() {
       )}
     </div>
   );
-
-  /*
-  return (
-    <div className="h-full flex flex-col">
-      <button
-        onClick={async () => {
-          const { questions: q } = await getQuestions(
-            "Messages during finals week."
-          );
-
-          setQuestions(q.questions);
-        }}
-      >
-        Generar Preguntas
-      </button>
-
-      <div className="flex flex-col justify-center items-center  grow space-y-5">
-        <p className="break-words p-0">{questions[0]}</p>
-        <button className="border-2 rounded-md border-double p-2 border-slate-300">
-          Next
-        </button>
-      </div>
-    </div>
-  );*/
 }
