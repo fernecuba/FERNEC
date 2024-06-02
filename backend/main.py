18756da205afae33ddcaa78703f20be848a3e56e
Cota <cotarelorodrigo@gmail.com>
2024-06-01 22:19:14 -0300

    add function to run with factory method

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 import os
 import uvicorn
-
+from loguru import logger
 from dependencies import parse_config, get_application
 
-
-# TODO: Set a logger
-if __name__ == "__main__":
+def create_app():
     config_path = os.getenv("CONFIG_FILE") if os.getenv("CONFIG_FILE") else "./config.yaml"
-    print(f"Use config file: {config_path}")
+    logger.info(f"Use config file: {config_path}")
     cfg = parse_config(config_path)
     app = get_application(cfg)
+    return app, cfg
+
+if __name__ == "__main__":
+    app, cfg = create_app()
     if app is None:
         raise TypeError("app not instantiated")
-    print('Starting FERNEC backend')
+    logger.info('Starting FERNEC backend')
     uvicorn.run(app, host=str(cfg.host), port=cfg.port)
