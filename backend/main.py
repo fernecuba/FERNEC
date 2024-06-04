fa2c2c87d47482eb6da938cfd562f9a894d77171
Nicolas Longo <nlongo@fi.uba.ar>
2024-06-03 23:07:46 -0300

    fixed merge with main

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import uvicorn
 from loguru import logger
 from dependencies import parse_config, get_application
@@ -6,6 +7,9 @@ from dependencies import parse_config, get_application
 config_path = os.getenv("CONFIG_FILE") if os.getenv("CONFIG_FILE") else "./config.yaml"
 print(f"Use config file: {config_path}")
 cfg = parse_config(config_path)
+logger.info(f"Use config file: {config_path}")
+logger.remove()
+logger.add(sys.stderr, level=cfg.log_level)
 app = get_application(cfg)
 if app is None:
     raise TypeError("app not instantiated")
