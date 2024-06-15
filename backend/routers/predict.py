57e3383c9c705aa3c9cd23e4cb57a9bbd92af818
Rodrigo Etchegaray <rorroeche@gmail.com>
2024-06-15 18:18:36 -0300

    Increased html size

--- backend/routers/predict.py
+++ backend/routers/predict.py
@@ -118,16 +118,16 @@ def send_email_with_prediction_results(result, user_email: str, request: Request
 
     body = f"""
             <html>
-                <body style="font-family: Arial, sans-serif; color: #333;">
+                <body style="font-family: Arial, sans-serif; color: #333; font-size: 18px;">
                     <div style="text-align: center; padding: 20px;">
-                        <img src="cid:logo" alt="FERNEC Logo" style="width: 150px;">
+                        <img src="cid:logo" alt="FERNEC Logo" style="width: 200px;">
                     </div>
                     <div style="margin: 20px; text-align: center;">
-                        <h2>Dear User,</h2>
-                        <p>We are pleased to inform you that your results are ready.</p>
-                        <p>Please click the link below to view them:</p>
-                        <a href="{url}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px;">View Results</a>
-                        <p>Sincerely,<br>The FERNEC Team</p>
+                        <h2 style="font-size: 24px;">Dear User,</h2>
+                        <p style="font-size: 20px;">We are pleased to inform you that your results are ready.</p>
+                        <p style="font-size: 20px;">Please click the link below to view them:</p>
+                        <a href="{url}" style="display: inline-block; padding: 12px 24px; background-color: #007BFF; color: #fff; text-decoration: none; border-radius: 5px; font-size: 20px;">View Results</a>
+                        <p style="font-size: 20px;">Sincerely,<br>The FERNEC Team</p>
                     </div>
                 </body>
             </html>
