b07ec769a60af15d646033ff1cc404039472c63d
Cota <cotarelorodrigo@gmail.com>
2024-06-29 23:22:36 -0300

    add workers to videos processor

--- backend/preprocessing/frames_generator/strategy/videos_processor/videos.py
+++ backend/preprocessing/frames_generator/strategy/videos_processor/videos.py
@@ -1,30 +1,49 @@
-import gc
+import os
 import cv2
-import ffmpeg
+import concurrent.futures
 from tqdm import tqdm
 from loguru import logger
 from preprocessing.frames_generator.face_detector.detector import detect_faces
 from preprocessing.frames_generator.strategy.images_processor.images import get_pixels, save_image
 from preprocessing.frames_generator.utils import clean_folder
 
+def read_frame(video_path, frame_number):
+    cap = cv2.VideoCapture(video_path)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    ret, frame = cap.read()
+    cap.release()
+    return frame_number, frame if ret else None
 
 # TODO: move me into videos_processor?
 def get_frames_from_video(video_path, frames_path, batch_size, channels, thumbnail_size, frames_order_magnitude, faces_only=False):
     cap = cv2.VideoCapture(video_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cap.release()
+
+    logger.info(f'FPS del video: {fps}')
+    logger.info(f'Total frames: {total_frames}')
+
+    num_cpus = os.cpu_count()
     raw_frames = []
     processed_count = 0
-    success, frame = cap.read()
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    logger.info(f'FPS del video: {fps}')
 
-    while success:
-        raw_frames.append(frame)
-        if len(raw_frames) == batch_size:
-            processed_count = detect_and_save_faces(frames_path, channels, thumbnail_size, frames_order_magnitude, faces_only, raw_frames, processed_count)
-            raw_frames = []
-        success, frame = cap.read()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_cpus) as executor:
+        future_to_frame = {executor.submit(read_frame, video_path, i): i for i in range(total_frames)}
 
-    cap.release()
+        for future in concurrent.futures.as_completed(future_to_frame):
+            frame_number = future_to_frame[future]
+            try:
+                frame_number, frame = future.result()
+                if frame is not None:
+                    
+                    raw_frames.append(frame)
+                    if len(raw_frames) == batch_size:
+                        processed_count = detect_and_save_faces(frames_path, channels, thumbnail_size, frames_order_magnitude, faces_only, raw_frames, processed_count)
+                        raw_frames = []
+            except Exception as exc:
+                logger.error(f"Frame {frame_number} generated an exception: {exc}")
+
 
     if len(raw_frames) > 0:
         processed_count = detect_and_save_faces(frames_path, channels, thumbnail_size, frames_order_magnitude, faces_only, raw_frames, processed_count)
