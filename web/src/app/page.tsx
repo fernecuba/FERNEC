527293475307cf768eba51b37bc2ebbc0a2bede1
Rodrigo Etchegaray <retchegaray@fi.uba.ar>
2024-07-11 20:04:11 -0300

    Contact Us button is a mail to FERNEC mailbox

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                 </Link>
                 <Link
                   className="inline-flex h-12 items-center justify-center rounded-md border border-gray-200 bg-white px-8 font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-                  href="#"
+                  href="mailto:fernec.fiuba@gmail.com"
                 >
                   Contact Us
                 </Link>
